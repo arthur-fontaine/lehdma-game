e8bc461e8547570fd5187fcca10ffcc129266865
arthur-fontaine <0arthur.fontaine@gmail.com>
2021-12-23 01:11:36 +0100

    fix: some costume names were wrong

--- utils/load_costumes.py
+++ utils/load_costumes.py
@@ -7,7 +7,12 @@ def load_costumes(costumes_directory: str):
     for file in os.listdir(costumes_directory):
         if file.endswith(".png"):
             filename = file.split(".")[0]
-            filename = "-".join(filename.split("-")[1::])
+
+            if filename[-1].isnumeric():
+                filename = "-".join(filename.split("-")[-2::])
+            else:
+                filename = filename.split("-")[-1]
+
             costumes[filename] = os.path.join(costumes_directory, file)
 
     return costumes
